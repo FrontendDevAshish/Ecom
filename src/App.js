67186a3acc48633a5f3b4ef0b8d53426c50cd30d
FrontendDevAshish <kumar.29ashish.95@gmail.com>
2023-08-22 11:08:16 +0530

    Routes setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router,Routes ,Route } from "react-router-dom"
+import Home from './Views/Home/Home';
+import About from './Views/About/About';
+import Contact from './Views/Contact/Contact';
+import Products from './Views/Products/Products';
+import SingleProduct from './Views/SingleProduct/singleProduct';
+import Cart from './Views/Cart/Cart';
+import ErrorPage from './Views/Error/Error';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+
+    {/* These are the main routes */}
+
+    <Routes>
+      <Route path = '/' element = {<Home/>}/>
+      {/* --Landing Page */}
+
+      <Route path ='/about' element = {<About/>} />
+      {/* --About page */}
+
+      <Route path ='/contact' element = {<Contact/>} />
+      {/* --Contact us */}
+
+      <Route path ='/products' element = {<Products/>} />
+      {/* --All Products */}
+
+      <Route path ='/single-product' element = {<SingleProduct/>} />
+      {/* Single Product */}
+
+      <Route path ='/cart' element = {<Cart/>} />
+      {/* Cart products */}
+
+      <Route path ='*' element = {<ErrorPage/>} />
+      {/* --This is the error page */}
+    </Routes>
+
+    
+  </Router>
   );
 }
+// products
+// contact
+// singleproduct
+// cart
+// error
 
 export default App;
